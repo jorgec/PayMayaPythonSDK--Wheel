08bcc8fd30b96185d4c71fc60f3a0b2d6a536f05
Jorge Cosgayon <jcosgayon@servio.ph>
2020-07-03 22:15:14 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-    name='PayMaya Python SDK',
+    name='PayMayaPythonSDK',
     version='0.1',
     scripts=['paymaya_sdk.py'],
     author="Jorge Cosgayon",
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A Python port of the PHP PayMaya SDK",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/jorgec/PayMaya-Python-SDK",
+    url="https://github.com/jorgec/PayMayaPythonSDK--Wheel",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
